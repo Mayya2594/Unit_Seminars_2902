90049480823e5edf6986444c3077f5f9b6708634
Mayya <majko2594@gmail.com>
2024-03-06 00:41:31 +0300

    add comments in Seminar_2_HW

--- Seminar_2_HW/Test/hw/VehicleTest.java
+++ Seminar_2_HW/Test/hw/VehicleTest.java
@@ -16,33 +16,39 @@ class VehicleTest {
         motorcycle = new Motorcycle("BMW", "F 850 GS", 2023);
     }
 
+    // Проверить, что экземпляр объекта Car также является экземпляром транспортного средства (используя оператор instanceof).
     @Test
     void carInstanceOfVehicle() {
         assertInstanceOf(Vehicle.class, car);
     }
 
+    // Проверить, что объект Car создается с 4-мя колесами.
     @Test
     void carHasFourWheels() {
         assertThat(car.getNumWheels()).isEqualTo(4);
     }
 
+    // Проверить, что объект Motorcycle создается с 2-мя колесами.
     @Test
     void motorcycleHasFourWheels() {
         assertThat(motorcycle.getNumWheels()).isEqualTo(2);
     }
 
+    // Проверить, что объект Car развивает скорость 60 в режиме тестового вождения (используя метод testDrive()).
     @Test
     void carSpeedIs60AtTestDrive() {
         car.testDrive();
         assertThat(car.getSpeed()).isEqualTo(60);
     }
 
+    // Проверить, что объект Motorcycle развивает скорость 75 в режиме тестового вождения (используя метод testDrive()).
     @Test
     void motorcycleSpeedIs75AtTestDrive() {
         motorcycle.testDrive();
         assertThat(motorcycle.getSpeed()).isEqualTo(75);
     }
 
+    // Проверить, что в режиме парковки (сначала testDrive, потом park, т.е. эмуляция движения транспорта) машина останавливается (speed = 0).
     @Test
     void carSpeedIs0AtPark() {
         car.testDrive();
@@ -50,6 +56,7 @@ class VehicleTest {
         assertThat(car.getSpeed()).isEqualTo(0);
     }
 
+    // Проверить, что в режиме парковки (сначала testDrive, потом park, т.е. эмуляция движения транспорта) мотоцикл останавливается (speed = 0).
     @Test
     void motorcycleSpeedIs0AtPark() {
         motorcycle.testDrive();
